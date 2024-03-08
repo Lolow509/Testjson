f98bf091478c3608d567c7ac3e462589438ded36
Lolow509 <162756735+Lolow509@users.noreply.github.com>
2024-03-08 23:20:19 +0100

    Create restaurantData.js

---
+++ restaurantData.js
@@ -0,0 +1,216 @@
+{
+  "Extra": {
+    "Accompagnement": {
+      "choix": false,
+      "listElement": [
+        {
+          "nom": "Salade",
+          "prix": "0"
+        },
+        {
+          "nom": "Frite",
+          "prix": "0"
+        },
+        {
+          "nom": "Aloko",
+          "prix": "2"
+        }
+      ],
+      "nomExtra": "Accompagnement",
+      "requis": "Obligatoire"
+    },
+    "Boisson": {
+      "choix": false,
+      "listElement": [
+        {
+          "nom": "Eau",
+          "prix": "0"
+        },
+        {
+          "nom": "Jus d'orange",
+          "prix": "0"
+        },
+        {
+          "nom": "Jus de pomme",
+          "prix": "0"
+        },
+        {
+          "nom": "Vin rouge",
+          "prix": "7"
+        },
+        {
+          "nom": "Vin blanc",
+          "prix": "7.30"
+        }
+      ],
+      "nomExtra": "Boisson",
+      "requis": "Obligatoire"
+    },
+    "Sauce": {
+      "choix": false,
+      "listElement": [
+        {
+          "nom": "hbihkb",
+          "prix": "10"
+        }
+      ],
+      "nomExtra": "Sauce",
+      "requis": "Obligatoire"
+    },
+    "baguette": {
+      "choix": true,
+      "listElement": [
+        {
+          "nom": "olive",
+          "prix": "3"
+        },
+        {
+          "nom": "Carotte",
+          "prix": "2"
+        }
+      ],
+      "nomExtra": "baguette",
+      "requis": "Obligatoire"
+    }
+  },
+"Menu": {
+  "Boisson": {
+    "Foie Gras": {
+      "categorie": "Boisson",
+      "description": "Un mets délicat à base de foie gras d'oie ou de canard, souvent préparé en terrine. Il est souvent accompagné de pain grillé ou de brioche, ainsi que de confiture de figues ou de chutney.",
+      "extra": [
+        "Liste Vide"
+      ],
+      "img": "https://restaurant.khaki.free.nf/wp-content/uploads/2023/11/89009f23-a3fa-406b-a09e-ff763b67379b.webp",
+      "nom": "Foie Gras",
+      "prix": "11"
+    },
+    "Soupe à l'Oignon Gratinee": {
+      "categorie": "Boisson",
+      "description": "Une soupe traditionnelle à base d'oignons caramélisés, de bouillon de bœuf, de vin blanc et d'herbes de Provence. Elle est généralement recouverte de fromage fondu sur des tranches de pain grillé",
+      "extra": [
+        "Liste Vide"
+      ],
+      "img": "https://restaurant.khaki.free.nf/wp-content/uploads/2023/11/4e17dfae-66b8-4b31-8a59-b0de6f34d62c.webp",
+      "nom": "Soupe à l'Oignon Gratinee",
+      "prix": "9"
+    },
+    "test2": {
+      "categorie": "Boisson",
+      "description": "ceci est une description",
+      "extra": [
+        "Liste Vide"
+      ],
+      "img": "test.png",
+      "nom": "test2",
+      "prix": 88
+    }
+  },
+  "Entrée": {
+  "Foie Gras": {
+    "categorie": "Entrée",
+    "description": "Un mets délicat à base de foie gras d'oie ou de canard, souvent préparé en terrine. Il est souvent accompagné de pain grillé ou de brioche, ainsi que de confiture de figues ou de chutney.",
+    "extra": ["Liste Vide"],
+    "img": "https://firebasestorage.googleapis.com/v0/b/menuresto-24cc5.appspot.com/o/fois_gras.jpg?alt=media&token=4e1458f2-9c8c-4718-b78c-78670b566a6a",
+    "nom": "Foie Gras",
+    "prix": "11"
+  },
+  "Soupe à l'Oignon Gratinee": {
+    "categorie": "Entrée",
+    "description": "Une soupe traditionnelle à base d'oignons caramélisés, de bouillon de bœuf, de vin blanc et d'herbes de Provence. Elle est généralement recouverte de fromage fondu sur des tranches de pain grillé",
+    "extra": ["Liste Vide"],
+    "img": "https://firebasestorage.googleapis.com/v0/b/menuresto-24cc5.appspot.com/o/Soupe-%C3%A0-l_Oignon-Gratinee.jpg?alt=media&token=cf2d9b6c-5e0d-40e7-b005-7d845bf761fd",
+    "nom": "Soupe à l'Oignon Gratinee",
+    "prix": "12"
+  },
+  "Salade Niçoise": {
+    "categorie": "Entrée",
+    "description": "Une salade méditerranéenne composée de thon, d'œufs durs, de haricots verts, de tomates, d'olives et de pommes de terre, le tout assaisonné d'une vinaigrette à l'huile d'olive",
+    "extra": ["Liste Vide"],
+    "img": "https://firebasestorage.googleapis.com/v0/b/menuresto-24cc5.appspot.com/o/Salade-Ni%C3%A7oise.jpg?alt=media&token=b5875fdc-13ec-4c4e-b781-1ed9d4ab5335",
+    "nom": "Salade Niçoise",
+    "prix": "15"
+  }
+},
+  "Plat": {
+  "Foie Gras": {
+    "categorie": "Plat",
+    "description": "Un mets délicat à base de foie gras d'oie ou de canard, souvent préparé en terrine. Il est souvent accompagné de pain grillé ou de brioche, ainsi que de confiture de figues ou de chutney.",
+    "extra": ["Liste Vide"],
+    "img": "https://restaurant.khaki.free.nf/wp-content/uploads/2023/11/89009f23-a3fa-406b-a09e-ff763b67379b.webp",
+    "nom": "Foie Gras",
+    "prix": "11"
+  },
+  "Soupe à l'Oignon Gratinee": {
+    "categorie": "Plat",
+    "description": "Une soupe traditionnelle à base d'oignons caramélisés, de bouillon de bœuf, de vin blanc et d'herbes de Provence. Elle est généralement recouverte de fromage fondu sur des tranches de pain grillé",
+    "extra": ["Liste Vide"],
+    "img": "https://restaurant.khaki.free.nf/wp-content/uploads/2023/11/4e17dfae-66b8-4b31-8a59-b0de6f34d62c.webp",
+    "nom": "Soupe à l'Oignon Gratinee",
+    "prix": "9"
+  },
+  "Test2": {
+    "categorie": "Plat",
+    "description": "ceci est une description du test 2",
+    "extra": ["Liste Vide"],
+    "img": "test.png",
+    "nom": "Test2",
+    "prix": 22
+  },
+  "Riz": {
+    "categorie": "Plat",
+    "description": "Un plat de riz super bon",
+    "extra": ["Boisson"],
+    "img": "https://firebasestorage.googleapis.com/v0/b/menuresto-24cc5.appspot.com/o/essai%2Friz.jpg?alt=media&token=a6dc177e-91c2-4201-94ef-a64125b77a5a",
+    "nom": "Riz",
+    "prix": "3"
+  },
+  "Test4": {
+    "categorie": "Plat",
+    "description": "ceci est une description du test 4",
+    "extra": ["Liste Vide"],
+    "img": "test.png",
+    "nom": "Test4",
+    "prix": 44
+  },
+  "Trésor": {
+    "categorie": "Plat",
+    "description": "tresor de kellogs",
+    "extra": ["Accompagnement"],
+    "img": "https://firebasestorage.googleapis.com/v0/b/menuresto-24cc5.appspot.com/o/essai%2Fcoffre-au-tresor.png?alt=media&token=f08244e3-768c-4a0d-b356-d7decdbcb351",
+    "nom": "Trésor",
+    "prix": "13"
+      }
+    }
+  },
+"informations": {
+    "id": "aPC8CKMXGeYvWX2kJsNzUzsNNnG2",
+    "paypal": {
+      "secretKey": "secretKey"
+    },
+    "stripe": {
+      "publicKey": "pk_test",
+      "secretKey": "sk_test"
+    }
+  },
+"listeMenu": [
+    "Entrée",
+    "Plat",
+    null,
+    null,
+    "Boisson"
+  ],
+"listeMontres": {
+    "O": {
+      "idPass": "CoKDA1u6LyG6fD",
+      "numéro": 61849
+    }
+  },
+"listeTable": [
+    "info",
+    "toutou",
+    "ziiip",
+    "oooooo",
+    "douu"
+  ]
+}
